bfd3c08530875903a5e7629c86735eb599f897cb
Paweł Feruś <panther.mindc@gmail.com>
2018-12-10 14:36:55 +0100

    put test

--- test/test.php
+++ test/test.php
@@ -6,8 +6,13 @@ $dotenv = new Dotenv\Dotenv(__DIR__ . '/../');
 $dotenv->load();
 
 $client = new \AGSystems\REST\Trello\Client(
-    getenv('TRELLO_TOKEN'),
-    getenv('TRELLO_APP_ID')
+    getenv('API_TOKEN'),
+    getenv('APP_ID')
 );
 
 var_export($client->members->me->boards->get());
+
+var_export($client->cards('xQ4gCVK8')->put([
+    'name' => 'TEST ' . uniqid(),
+    'desc' => 'Description ' . date('Y-m-d H:i:s'),
+]));
