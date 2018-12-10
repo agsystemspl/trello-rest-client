00909848bcb1b90907f8058ad4a05edd7f8ad6fe
Paweł Feruś <panther.mindc@gmail.com>
2018-12-10 12:53:48 +0100

    gitignore

---
+++ src/Client.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: panther
+ * Date: 10.12.2018
+ * Time: 12:14
+ */
+
+class Client
+{
+
+}
---
+++ test/test.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: panther
+ * Date: 10.12.2018
+ * Time: 12:44
+ */
